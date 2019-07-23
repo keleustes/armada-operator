8580eae94ca94b550c0554f5b9e45ef8210dd96a
Jerome Brette <jbrette@gmail.com>
2019-07-23 15:08:23 -0500

    Update to go.mod to Kubernetes 1.15.1

--- pkg/apis/armada/v1alpha1/armadachart_types.go
+++ pkg/apis/armada/v1alpha1/armadachart_types.go
@@ -60,7 +60,7 @@ type ArmadaChartValues struct {
 	// kube_service contains tbd
 	KubeService *AVKubeService `json:"kube_service,omitempty"`
 	// labels contains tbd
-	// +k8s:openapi-gen=false
+	// +k8s:openapi-gen=true
 	Labels *map[string]ArmadaMapString `json:"labels,omitempty"`
 	// livenessProbe contains tbd
 	Livenessprobe *AVLivenessprobe `json:"livenessProbe,omitempty"`
